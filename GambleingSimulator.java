300cd4b8af6c61f1009835e51ab12e4bbd99cfa4
vishal5394 <vishaljadyal@gmail.com>
2022-08-07 15:22:08 +0530

    Added:Gambler Win or Lost Would resign for the day

--- GambleingSimulator.java
+++ GambleingSimulator.java
@@ -9,29 +9,36 @@ public class GambleingSimulator{
 	static int LOST=0;
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Starting amount of stake : $ "+AMOUNT_OF_STAKE);
-		 System.out.println("Amount to bet after every play : $"+BET_AMOUNT);
-		 int play = (int) Math.floor(Math.random() * 10) % 2;
-		 System.out.println("Gamblers play:"+play);
+		 System.out.println("Starting amount of stake is $"+AMOUNT_OF_STAKE);
+		 System.out.println("Amount to bet after every play $"+BET_AMOUNT);
 		 int InHandcash = AMOUNT_OF_STAKE;
-		 switch(play) {
-		 	case 1:
-		 		 InHandcash = InHandcash + BET_AMOUNT;
-		 		 System.out.println("Total current cash gambler has $"+InHandcash);
-		 		 break;
-		 	case 2:
-		 		 InHandcash = InHandcash - BET_AMOUNT;
-		 		 System.out.println("Total current cash gambler has $"+InHandcash);
-		 		 break;
-		 	default:
-		 		 System.out.println("default");
+		 while(InHandcash> 50 && InHandcash < 150) {
+			 int play = (int) Math.floor(Math.random() * 10) % 2;
+			 System.out.println("\nGamblers play:"+play);
+		 
+			 switch(play) {
+		 		case 1:
+		 			InHandcash = InHandcash + BET_AMOUNT;
+		 			System.out.println("Total current cash gambler has $"+InHandcash);
+		 			break;
+		 		case 0:
+		 			InHandcash =InHandcash - BET_AMOUNT;
+		 			System.out.println("Total current cash gambler has $"+InHandcash);
+		 			break;
+		 		default:
+		 			System.out.println("default");
+			 }
+			 if(play == 1) {
+				 System.out.println("Player won the current play");
+			 }
+			 else {
+				 System.out.println("Player lost the current play");
+			 }
+		 
 		 }
-		 if(play == 1) {
-			 System.out.println("Player had won the first play");
+		 if(InHandcash==150 || InHandcash==50) {
+			 System.out.println("Player will resign for current day");
+			 
 		 }
-		 else {
-			 System.out.println("Player had lost the first play");
-		 }
-
 	}
 }
